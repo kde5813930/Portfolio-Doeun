37416ebf619c366eebdc9942b6feac642423d026
kde5813930 <kde5813@gmail.com>
2023-07-26 01:39:41 +0900

    feat : about 섹션 퍼블리싱 (진행중)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const header = document.querySelector(".header")
+
+window.addEventListener("scroll", () => {
+    let scrollTop = window.scrollY;
+
+    if (scrollTop === 0){
+        header.classList.remove("blur");
+    } else{
+        header.classList.add("blur");
+    }
+
+
+});
