f8cd7937319b8bb374683eb82c9342405eb35b45
kde5813930 <kde5813@gmail.com>
2023-09-06 16:27:48 +0900

    feat : 반응형 작업 (진행중)

--- js/main.js
+++ js/main.js
@@ -14,6 +14,35 @@ window.addEventListener("scroll", () => {
 
 });
 
+// 헤더 모바일 햄버거버튼 클릭시 이벤트
+const hamburgerBtn = document.querySelector(".btn-hamburger");
+
+function toggleClass(element, className) {
+    if(element.classList.contains(className)){
+        element.classList.remove(className);
+    }else{
+        element.classList.add(className);
+    };
+}
+hamburgerBtn.addEventListener('click', function(){
+    toggleClass($html, 'lock');
+    toggleClass(header, 'active');
+    toggleClass(hamburgerBtn, 'active');
+});
+
+// 헤더 모바일 gnb list 클릭시 이벤트
+const gnbList = document.querySelectorAll(".gnb-list li");
+
+    gnbList.forEach((nav) => {
+        nav.addEventListener("click", () => {
+            if ( header.classList.contains("active")){
+                header.classList.remove("active");
+            }
+        });
+    })
+
+
+
 
 //모달 close
 function modalClose(){
@@ -44,4 +73,41 @@ document.addEventListener("click", (e) => {
     modalCloseKeyDown();
 
 });
+
+
+//go-top
+function goTop() {
+
+    const goTopWrap = document.querySelector(".go-top-wrap");
+    const goTop = document.querySelector(".go-top");
+
+    const domHei = document.offsetHeight;
+    const winHei = document.offsetHeight;
+    const footerHei = document.querySelector(".footer").clientHeight;
+    const footerTop = domHei - winHei - footerHei;
+
+    /* scroll 시  go-top show, hide */
+    window.addEventListener("scroll", () => {
+        const scrollTop = $(window).scrollTop();
+
+        if (scrollTop > 0) {
+            goTopWrap.classList.add("active");
+        } else {
+            goTopWrap.classList.remove("active");
+        }
+
+        if (scrollTop >= footerTop) {
+            goTopWrap.classList.add("bottom");
+        } else {
+            goTopWrap.classList.remove("bottom");
+        }
+    })
+
+    //goTop 클릭시 화면 상단으로 이동
+    goTop.addEventListener("click", () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+}
+
+goTop();
 
